be2a2f105d892f273bc8a0eb92dac80a56d6d9aa
rvelos <43866232+rvelos@users.noreply.github.com>
2018-11-26 00:49:57 -0200

    25-11-2018

--- acai p. integrador/js/index.js
+++ acai p. integrador/js/index.js
@@ -5,8 +5,9 @@ $("#curiosidades").click(function(){
 		$("main").slideDown(2000)
 		$("#cur").css("display", "flex"),
 		$("table").css("display", "none")
-		
-	}  );
+	}  
+	);
+
 $("footer").click(function(){ 
 		$("main").slideUp(2000)
 		
@@ -16,13 +17,32 @@ $("footer").click(function(){
 
 $("#galeria").click(function(){
 	$("main").slideDown(400),
-	$("main table").css("display", "flex"),
-		$("main #cur").css("display", "none")
+	$("main table, tr").css("display", "flex"),
+		$("main #cur").css("display", "none"),
+		    $.getJSON('json/dados.json', function(json) {
+                 $("tr").append(`
+            <td><img src="${dados.imagens}"></td>
+            `)
+          console.log(dados.imagens);
+    
+      });
 });
 
 
 
-});
+
+
+//Valor retornado: Verde Amarelo Azul Branco
+cores = ["Verde", "Amarelo", "Azul", "Branco"];
+ 
+function imprimir(item) {
+    console.log(item); 
+}
+ 
+cores.forEach(imprimir);
+
+ });
+
 
 
 	
